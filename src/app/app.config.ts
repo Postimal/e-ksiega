b7339765b27ac368e2fa363336f932902d7fe27c
Michał Postek <michalpostek@mam-sdf.local>
2026-09-13 20:12:31 +0200

    minor changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './routing/app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+      }),
+    ),
+  ],
 };
